4630b695c6617a35ee8761e20043728587117cf2
Alexander Laties <alex@laties.info>
2021-02-10 01:25:23 -0500

    begin transitioning to modern stack

---
+++ cmd/gotheater/main.go
@@ -0,0 +1,12 @@
+package main
+
+/**
+A webserver that provides access to:
+ - User management
+ - Room management
+ - Content management
+ - Transcode management
+*/
+func main() {
+	return
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alex-laties/gotheater
+
+go 1.15
